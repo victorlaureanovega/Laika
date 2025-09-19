c9b4941118c280bbad6867461c41c69aa9be6a96
Victor <victorlaureanovega@gmail.com>
2025-09-19 17:48:06 -0600

    Rediseño del sitio con Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('laika');
+
+  abrirPDF() {
+    window.open("El cielo de Laika.pdf", "_blank");
+  }
 }
